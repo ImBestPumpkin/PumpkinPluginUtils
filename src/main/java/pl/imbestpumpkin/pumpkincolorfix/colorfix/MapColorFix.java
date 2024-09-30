b0520969a1c2525bb28df8f3e02a1a838513d003
Karol Urbański <70109241+ImBestPumpkin@users.noreply.github.com>
2024-09-30 08:40:46 +0200

    Version 1.1
    Version 1.1

--- src/main/java/pl/imbestpumpkin/pumpkincolorfix/colorfix/MapColorFix.java
+++ src/main/java/pl/imbestpumpkin/pumpkincolorfix/colorfix/MapColorFix.java
@@ -1,20 +1,12 @@
 package pl.imbestpumpkin.pumpkincolorfix.colorfix;
 
 import org.bukkit.ChatColor;
-import pl.imbestpumpkin.pumpkincolorfix.colorfix.Exception.ValueOfMapIsNotStringException;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class MapColorFix {
-    public static Map<Object, String> colorOf(Map<Object, Object> map) throws ValueOfMapIsNotStringException {
-        for (Map. Entry<Object, Object> entry : map.entrySet()) {
-            if (!(entry.getValue() instanceof String)) {
-                throw new ValueOfMapIsNotStringException();
-            } else {
-                break;
-            }
-        }
+    public static Map<Object, String> colorOf(Map<Object, String> map){
 
         Map<Object, String> coloredMap = new HashMap<>();
         map.forEach((k, v) -> {
